b07afc3c11a3ff743b4ab1fff58b26bf05fdf7dc
Martin Becke <becke@TDRMacBook1.local>
2011-07-11 17:17:38 +0200

    add comment

--- src/transport/tcp/TCPMultipath.cc
+++ src/transport/tcp/TCPMultipath.cc
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program; if not, see <http://www.gnu.org/licenses/>.
 //
-
+//
 
 #include "TCP.h"
 #include "TCPMultipath.h"
