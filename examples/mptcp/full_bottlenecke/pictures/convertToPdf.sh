c674a21c117cb4d179b0ab1bd7f76d54e37283e6
Martin Becke <Martin.Becke@uni-due.de>
2013-07-11 21:02:40 +0200

    cut 11.07.13

--- examples/mptcp/full_bottlenecke/pictures/convertToPdf.sh
+++ examples/mptcp/full_bottlenecke/pictures/convertToPdf.sh
@@ -3,12 +3,15 @@
 FILTER=$1
 echo "Convert all *.svg to pdf"
 echo "be sure librsvg2-bin packet is installed"
-dateien=`find ../$FOLDER -name "*"$FILTER"*" -a -name "_*.svg" -type f`
+dateien=`find ../$FOLDER -name "*"$FILTER"*" -a -name "*.svg" -type f`
 rm -f *.pdf 
 for i in $dateien ; do 
-        echo "process.."
+        echo "process.." $i
         /usr/bin/rsvg-convert -f pdf  -o $i.pdf $i 
 done
 echo "created:"
 ls -l *.pdf
+echo "create one pdf with all"
+dateien=`find ../$FOLDER -name "*"$FILTER"*" -a -name "*.pdf" -type f`
+/usr/bin/gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=all_$FILTER.pdf -dBATCH $dateien
 echo "done"
