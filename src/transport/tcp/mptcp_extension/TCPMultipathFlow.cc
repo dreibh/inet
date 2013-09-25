00309af2bf9d0caee502b9c7907eb14f1db56efc
becke <martin.becke@uni-due.de>
2013-09-25 18:13:06 +0200

    minor - but important

--- src/transport/tcp/mptcp_extension/TCPMultipathFlow.cc
+++ src/transport/tcp/mptcp_extension/TCPMultipathFlow.cc
@@ -694,11 +694,12 @@ void  MPTCP_Flow::_opportunisticRetransmission(TCPConnection* sub){
     uint64 old_mptcp_snd_nxt = mptcp_snd_nxt;
     uint64 old_mptcp_highestRTX = mptcp_highestRTX;
     if(!sub->dss_dataMapofSubflow.empty()){
-        if(sub->dss_dataMapofSubflow.begin()->second->dss_seq == mptcp_snd_una){
+       //std::cerr <<  sub->dss_dataMapofSubflow.begin()->second->dss_seq << " "<<  mptcp_snd_una << std::endl;
+        if(sub->dss_dataMapofSubflow.begin()->second->dss_seq -1 == mptcp_snd_una){
             return;
         }
     }
-    if(mptcp_highestRTX -1 == mptcp_snd_nxt){
+    if(mptcp_highestRTX - 1 == mptcp_snd_nxt){
         return;
     }
     if(mptcp_highestRTX <= mptcp_snd_una){
